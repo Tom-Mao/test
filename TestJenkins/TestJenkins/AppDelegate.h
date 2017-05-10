87032b79f9976858fe069bf509fb980444ce7c65
Tom-Mao <Tom.Mao@wolaidai.com>
2017-05-10 15:53:16 +0800

    text

---
+++ TestJenkins/TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by Tom on 2017/5/10.
+//  Copyright © 2017年 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
